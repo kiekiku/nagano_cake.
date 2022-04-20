5aa22d388a52ecd94a60456dbabfc4c324ba885f
kiekiku <hsmny1022@gmail.com>
2022-04-20 14:54:27 +0000

    コントローラ全て

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,66 +1,44 @@
 class Admin::ItemsController < ApplicationController
-#ログインユーザーのみproduct#indexは閲覧可
-  before_action :authenticate!, except: [:index]
-#退会済みユーザー
-  before_action :user_is_deleted, except: [:index]
+  before_action :authenticate_admin!
+  def index
+    @items = Item.all
+  end
 
   def new
     @item = Item.new
   end
 
-  def edit
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      flash[:success] = "登録に成功しました"
+      redirect_to admin_items_path
+    else
+      flash[:warning] = "入力内容を確認してください"
+      render :new
+    end
   end
 
-#商品一覧ページ
-	def index
-        if params["genre"]
-        	@items = Item.active.where(genre_id: params["genre"])
-        else
-			    @items = Item.active
-        end
-          @genres = Genre.active
+  def show
+    @item = Item.find(params[:id])
+  end
 
-	end
+  def edit
+    @item = Item.find(params[:id])
+  end
 
-# 顧客側の商品詳細ページ
-	def show
-		@item = Item.find(params[:id])
-		@cart_item = CartItem.new(item_id: @item.id)
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      flash[:success] = "更新に成功しました"
+      redirect_to admin_items_path
+    end
+  end
 
-	#管理者は買い物はできないが、詳細ページの閲覧はできる。
-		if current_user.nil?
-			items = CartItem.all
-		else
-			items = current_user.cart_items
-		end
-
-	#既にカートに商品が入っているときは、商品一覧ページに戻る
-		if items.pluck(:product_id).include?(@cart_item.product_id)
-			flash[:warning] = "その商品はカートに入っています"
-			redirect_to root_path
-		end
-	end
-
+  private
+    def item_params
+      params.require(:product).permit(:name, :image, :price, :introduction, :genre_id, :is_stopped)
+    end
 end
-	private
 
-#ストロングパラメーター
-	def product_params
-		params.require(:product).permit(:image_id, :name, :introduction)
-	end
-
-#adminでなければuserの中で振り分ける
-    def authenticate!
-      if admin_signed_in?
-      else
-      	authenticate_user!
-      end
-    end
-
-#退会済みユーザーへの対応
-    def user_is_deleted
-      if user_signed_in? && current_user.is_deleted?
-         redirect_to root_path
-      end
-    end
 
