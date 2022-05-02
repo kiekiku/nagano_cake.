ecf42ab36341d555a27c037f36ccdc4855e614ef
kiekiku <hsmny1022@gmail.com>
2022-05-02 15:03:27 +0000

    会員情報編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -38,7 +38,7 @@ class Admin::CustomersController < ApplicationController
 
   private
   	def customer_params
-  		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :phone, :email, :is_deleted)
+  		params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :phone, :email, :is_active)
   	end
 end
   
